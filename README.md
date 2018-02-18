187889b060e8283139b76dcbb3c60a0aacd0c097
emanuele-f <black.silver@hotmail.it>
2018-02-18 11:02:24 +0100

    Initial arpreply commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# arpreply
+
+A tool to politely respond to ARP requests.
+
+This tool is not intended to perform ARP poisoning. It waits for ARP requests for
+a given IP address and reply with the specified MAC address. This comes handy in
+different situations, see examples below.
+
+## Use case: Pc with multiple addresses on the same local network range
+
+With linux we cannot assign two IP addresses belonging to the same network range
+to multiple interfaces on the same host as it makes routing to that network
+ambiguous as the system does not know which interface to use to deliver a packet
+to that network.
+
+We can use this tool to easily provide an additional IP address to the host to
+receive packets destinated to it.
+
+## Use case: Forward connections to remote L3 VPN client
+
+Supposing we have a VPN server (10.7.0.1) on a local network 192.168.1.0/24 and we are connecting
+to it remotely with our client whose ip on the VPN is 10.0.0.2.
+
+We want the clients on the network 192.168.1.0/24 to reach our 10.0.0.2 client.
+Traditionally we would need to setup a VPN server in L2 bridge mode, but this
+is a complicate setup and it requires changing the VPN server interfaces configuration.
+We can achieve the same effect on a L3 VPN server with this tool.
+
+Supposing that we want our local network clients to connect to our remote host using IP
+address 192.168.1.10, on the vpn server we run:
+
+```
+  sysctl -w net.ipv4.ip_forward=1
+  iptables -t nat -A PREROUTING -d 192.168.1.10 -j DNAT --to-dest 10.0.0.2
+  iptables -t nat -A POSTROUTING -d 10.0.0.2 -j SNAT --to-source 10.7.0.1
+  arpreply -i eth0 -a 192.168.1.10
+```
+
+The arpreply tool will essentially fake the 192.168.1.10 presence on the network,
+by redirecting all the incoming connections to the VPN server itself. On the
+VPN server, the IPTABLES rules will nat the incoming traffic to our remote host.
+
+Now anyone on the 192.168.1.0/24 network can connect to our remote host! For example,
+192.168.1.25 could run `ssh 192.168.1.10`, and its request will be forwarded through
+the VPN server to our remote host.
+
+## Run arpreply
+
+The package `python2` and `scapy` are required in order to run this program.
+
+```
+usage: arpreply [-h] --interface IFACE --ip-address ADDR [--mac MAC]
+
+Respond to ARP requests by forging MAC address
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --interface IFACE, -i IFACE
+                        Interface to listen ARP request on
+  --ip-address ADDR, -a ADDR
+                        The IP address to reply for
+  --mac MAC, -m MAC     Spoofed MAC address to send on replies. If empty,
+                        the interface MAC address will be used
+```
+
+In the simple case we just run something like `arpreply -i eth0 -a 192.168.1.10`.
---
+++ arpreply.py
@@ -0,0 +1,88 @@
+#!/bin/env python2
+#
+# arpreply
+# (C) 2018 Emanuele Faranda
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from scapy.all import *
+import fcntl, socket, struct
+import argparse
+import logging
+
+# https://stackoverflow.com/questions/159137/getting-mac-address
+def getHwAddr(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+    return ':'.join(['%02x' % ord(char) for char in info[18:24]])
+
+def packetLoop(iface, reply_mac, responder_address):
+  logging.info("Starting packet poll loop")
+
+  while True:
+    packets = sniff(filter="arp and host " + responder_address, count=1)
+    #packets.summary()
+    arp_request = packets[0]
+    #arp_request.show()
+
+    client_mac = arp_request.hwsrc
+    client_ip = arp_request.psrc
+
+    logging.info("ARP request: %s (%s) asks who is %s, reply %s" % (client_ip, client_mac, responder_address, reply_mac))
+
+    reply = Ether()/ARP()
+    reply.op = 2
+    reply.psrc = responder_address
+    reply.pdst = client_ip
+    reply.hwdst = client_mac
+    reply.hwsrc = reply_mac
+    reply[Ether].src = reply_mac
+    reply[Ether].dst = client_mac
+    #reply.show()
+    sendp(reply, verbose=False, iface=iface)
+
+def parseArgs():
+  parser = argparse.ArgumentParser(
+    prog="arpreply",
+    description="Respond to ARP requests by forging MAC address",
+    formatter_class=argparse.RawTextHelpFormatter,
+    epilog='''example:
+
+ Respond to IP address 192.168.1.10 ARP requests with eth0 interface mac address
+   sudo ./arpreply.py -i eth0 -a 192.168.1.10''')
+
+  parser.add_argument("--interface", "-i", dest = "iface",
+    help="Interface to listen ARP request on", type=str, required=True)
+  parser.add_argument("--ip-address", "-a", dest = "addr",
+    help="The IP address to reply for", type=str, required=True)
+  parser.add_argument("--mac", "-m", dest = "mac",
+    help="Spoofed MAC address to send on replies. If empty,\nthe interface MAC address will be used", type=str, default="")
+
+  return parser.parse_args()
+
+if __name__ == "__main__":
+  args = parseArgs()
+
+  logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
+  mac = args.mac
+
+  if not mac:
+    try:
+      mac = getHwAddr(args.iface)
+    except IOError:
+      logging.error("Could not get interface %s mac address, please specify it manually (-m option)" % args.iface)
+      exit(1)
+
+  packetLoop(args.iface, mac, args.addr)
